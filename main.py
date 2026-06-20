fa3b814ddfda3022f605c57f9b29180a113adf3d
Mega-333-Evolutions <vaoi77161@gmail.com>
2026-06-21 00:37:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,46 @@ async def chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def pre_init():
+import logging
+import os
+import sys
+import time
+from zoneinfo import ZoneInfo
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    Defaults,
+    CallbackQueryHandler,
+    ContextTypes,
+    AIORateLimiter,
+    InlineQueryHandler,
+)
+
+import constants as c
+import resources.Environment as Env
+from src.chat.manage_message import (
+    manage_regular as manage_regular_message,
+    manage_callback as manage_callback_message,
+)
+from src.service.message_service import full_message_send
+from src.service.timer_service import set_timers
+
+
+async def chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Send chat id of current chat
+    :param update: Telegram update
+    :param context: Telegram context
+    :return: None
+    """
+    await full_message_send(context, str(update.effective_chat.id), update)
+
+
+def pre_init():
     """
     Pre init checks
     :return: None, raises Exception if something is wrong
@@ -114,7 +154,7 @@ def main() -> None:
         .token(Env.BOT_TOKEN.get())
         .post_init(post_init)
         .defaults(defaults)
-        .rate_limiter(AIORateLimiter())
+        .rate_limiter(AIORateLimiter(max_retries=5))
         .connect_timeout(20)
         .read_timeout(60)
         .write_timeout(60)
@@ -137,6 +177,14 @@ def main() -> None:
 
     # Activate timers
     logging.getLogger("apscheduler.executors.default").propagate = False
+
+    application.run_polling(drop_pending_updates=Env.BOT_DROP_PENDING_UPDATES.get_bool())
+
+
+if __name__ == "__main__":
+    main()
+
+g.getLogger("apscheduler.executors.default").propagate = False
 
     application.run_polling(drop_pending_updates=Env.BOT_DROP_PENDING_UPDATES.get_bool())
 
