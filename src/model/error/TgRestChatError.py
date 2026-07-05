72c3b108be9db158178bdddc60223012aa9f5ed5
Mega-333-Evolutions <vaoi77161@gmail.com>
2026-07-05 18:02:34 +0530

    Replace phrase constant with string literal

--- src/model/error/TgRestChatError.py
+++ src/model/error/TgRestChatError.py
@@ -1,4 +1,3 @@
-import resources.phrases as phrases
 from src.model.error.Error import Error
 from src.model.error.ErrorSource import ErrorSource
 
@@ -6,4 +5,4 @@ from src.model.error.ErrorSource import ErrorSource
 class TgRestChatError(Error):
     source = ErrorSource.TG_REST
 
-    UNKNOWN_PREDICTION_ACTION = Error(1, phrases.UNKNOWN_PREDICTION_ACTION, source)
+    UNKNOWN_PREDICTION_ACTION = Error(1, "UNKNOWN_PREDICTION_ACTION", source)
